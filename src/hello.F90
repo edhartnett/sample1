d83fbaa96e5c68be7a7e9c3b43fc2336f982f33c
Ed <edwardjameshartnett@gmail.com>
2023-06-02 15:24:16 -0600

    Add fizzbuzz

--- src/hello.F90
+++ src/hello.F90
@@ -1,3 +1,13 @@
 program hello
+    implicit none
+    integer :: i
+
+    do i=1,10
+        print *,i
+        if (mod(i,3) == 0) print *,'Fizz!'
+        if (mod(i,5) == 0) then
+            print *,'Buzz!'
+        end if
+    end do
     print *,'Hello World!'
 end program
